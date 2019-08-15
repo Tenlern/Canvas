b435d9407dc36e6ef17de69096b3081fc03af410
Ilya Lyashenko <illysh20009@yandex.ru>
2019-08-15 19:51:06 +0300

    Fix Location entity
    + Relations with Node entity

--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -17,47 +17,25 @@ class Location
     private $id;
 
     /**
-     * @ORM\Column(type="integer")
-     */
-    private $node1_id;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $node2_id;
-
-    /**
      * @ORM\Column(type="string", length=127)
      */
     private $name;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Node")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $node1_id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Node")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $node2_id;
+
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getNode1Id(): ?int
-    {
-        return $this->node1_id;
-    }
-
-    public function setNode1Id(int $node1_id): self
-    {
-        $this->node1_id = $node1_id;
-
-        return $this;
-    }
-
-    public function getNode2Id(): ?int
-    {
-        return $this->node2_id;
-    }
-
-    public function setNode2Id(int $node2_id): self
-    {
-        $this->node2_id = $node2_id;
-
-        return $this;
     }
 
     public function getName(): ?string
@@ -68,6 +46,30 @@ class Location
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNode1Id(): ?Node
+    {
+        return $this->node1_id;
+    }
+
+    public function setNode1Id(?Node $node1_id): self
+    {
+        $this->node1_id = $node1_id;
+
+        return $this;
+    }
+
+    public function getNode2Id(): ?Node
+    {
+        return $this->node2_id;
+    }
+
+    public function setNode2Id(?Node $node2_id): self
+    {
+        $this->node2_id = $node2_id;
 
         return $this;
     }
